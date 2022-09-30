7d78633241a1c6cb2e6a6c60f940923f90602a31
Umar Qureshi <114771252+TheRealUmarQureshi@users.noreply.github.com>
2022-09-30 19:52:56 +0100

    Add files via upload

---
+++ Basic portfolio website template/test_subject.js
@@ -0,0 +1,5 @@
+/*THIS CODE IS FOR CLICKING THE BUTTON AND ACTIVATING THE ALERT, BUT YOU CAN CHANGE IT IF YOU KNOW JAVASCRIPT.*/
+var main_button = document.getElementById("button1")
+main_button.addEventListener("click",function(){
+    alert("whassup?")
+})
